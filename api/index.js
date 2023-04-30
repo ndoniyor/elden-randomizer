4de5da8e10233728596afa66630c824faa7cb414
ndoniyor <dnimatullo@gmail.com>
2023-04-30 12:13:41 -0400

    created api route for build generation

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser')
+const BuildController = require("./controllers/BuildController")
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json())
+app.use(cors({
+    origin: '*'
+}))
+
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+)
+
+app.get('/', (req,res)=>{
+    res.send('get?')
+})
+
+app.post('/api', BuildController.generateBuild);
+
+
+app.listen(port, ()=>{
+    console.log("Listening...");
+})
