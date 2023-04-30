b5a0367d632b007c397b644e603aef6f3ee7deb3
ndoniyor <dnimatullo@gmail.com>
2023-04-30 13:16:11 -0400

    added armor gen in controller

--- api/controllers/BuildController.js
+++ api/controllers/BuildController.js
@@ -13,31 +13,38 @@ const getRandomMagicType = () => {
 
 //issue here, when i put this all in a function, the response is weird
 const getMeleeWeapons = async (meleeType) => {
+    let weaponsList = [];
     let weapons = [];
     const type = (meleeType === "rand") ? getRandomMeleeType() : meleeType
     switch (type) {
         case "powerstance":
-            weapons.push(await build.getWepsSameTyped('weapon1."type" = weapon2."type"'));
+            console.log("ps")
+            weaponsList.push(await build.getWepsSameTyped('weapon1."type" = weapon2."type"'));
             break;
         case "dualwield":
-            weapons.push(await build.getWepsSameTyped('weapon1."type" != weapon2."type"'));
+            console.log("dw")
+            weaponsList.push(await build.getWepsSameTyped('weapon1."type" != weapon2."type"'));
             break;
         case "shield":
-            weapons.push(await build.getItem("shields"));
+            weaponsList.push(await build.getItemsDiffTable("weapons", "shields"));
+            break
         case "singlewield":
-            weapons.push(await build.getItem("weapons"));
+            weaponsList.push(await build.getItem("weapons"));
             break;
     }
-    console.log("wep1: ", weapons.rows)
-    return weapons;
+    return weaponsList;
 }
 
-exports.generateBuild = async (req, res) => {
+const generateBuild = async (req, res) => {
     let completedBuild = {};
     const params = req.body;
 
     //weapon generation
-    console.log(params)
     const weapons = await getMeleeWeapons(params.meleeType);
+    const armor = (params.armorSets) ? (await build.getItem("armor_sets")) : (await build.getDiffArmors());
+    //console.log(weapons)
     res.send(weapons)
 }
+
+
+module.exports = {generateBuild}
