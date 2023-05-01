a4fda007b49e5cfa46bbe83bdfa413f627b266e9
ndoniyor <dnimatullo@gmail.com>
2023-05-01 13:48:28 -0400

    added state hook 4 build (response from api call)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,27 +9,32 @@ import MainCard from './components/MainCard';
 
 function App() {
     const [flags, setFlags] = useState([]);
-
-    const getBuild = async () => {
-        console.log({
-            method: 'POST',
-            body: JSON.stringify(flags)
-          })
-        fetch("http://localhost:3000/api", {
-          method: 'POST',
-          headers: {
-            "Content-Type" : "application/json"
-          },
-          body: JSON.stringify(flags)
-        })
-    }
+    const [build, setBuild] = useState({});
 
     useEffect(() => {
+        console.log(build)
+    }, [build])
+
+    const getBuild = async () => {
+        fetch("http://localhost:3000/api", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(flags)
+        })
+            .then(response => response.json())
+            .then(data => {
+                setBuild(data)
+            })
+    }
+    useEffect(()=>{
         getBuild();
-    }, [flags])
+    },[flags])
 
     return (
         <div className='App'>
+            <h1 className="title">Elden Ring Build Randomizer</h1>
             <FormCard biasFlags={setFlags} />
             {/* <MainCard />
             <ArmorCard />
