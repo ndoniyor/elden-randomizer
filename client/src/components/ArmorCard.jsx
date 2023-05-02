69cdbd907e2fd02d7c471b61bf12e1d86830f3b3
ndoniyor <dnimatullo@gmail.com>
2023-05-02 16:47:32 -0400

    added component to display armor

--- client/src/components/ArmorCard.jsx
+++ client/src/components/ArmorCard.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import GridItem from './GridItem';
+import { Container, Row, Col } from 'react-bootstrap';
 
-export default function ArmorCard() {
+import "./styles/css/ArmorCard.css";
+
+export default function ArmorCard(props) {
+  const [armor, setArmor] = useState([])
+
+  useEffect(() => {
+    console.log(props.armor)
+    setArmor(props.armor);
+  }, [props])
+
   return (
-    <div>ArmorCard</div>
+    <Container>
+      <Row>
+        {armor.name ?
+          <Col>
+            <GridItem
+              name={armor.name}
+              image={armor.image}
+            />
+          </Col> : 
+          Object.keys(armor).length > 0 &&
+          armor.map((armorPiece) => {
+            return (
+              <Col>
+                <GridItem
+                  name={armorPiece.name}
+                  image={armorPiece.image}
+                />
+              </Col>)
+          })
+        }
+      </Row>
+    </Container>
   )
 }
