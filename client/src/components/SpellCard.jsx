3ee12e7cddb351ed7ad0e6101e5c9a08213013d1
ndoniyor <dnimatullo@gmail.com>
2023-05-02 16:48:13 -0400

    Added component to display spells

--- client/src/components/SpellCard.jsx
+++ client/src/components/SpellCard.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import GridItem from './GridItem';
+import { Container, Row, Col } from 'react-bootstrap';
 
-export default function SpellCard() {
+export default function SpellCard(props) {
+  const [spells, setSpells] = useState([])
+
+  useEffect(() => {
+    setSpells(props.spells);
+    console.log("spells:", spells)
+  }, [props])
+
   return (
-    <div>SpellCard</div>
+    <Container>
+      <Row>
+        {
+          Object.keys(spells).length > 0 &&
+          spells.map((spell) => {
+            return (
+              <Col>
+                <GridItem
+                  name={spell.name}
+                  image={spell.image}
+                />
+              </Col>)
+          })
+        }
+      </Row>
+    </Container>
   )
 }
