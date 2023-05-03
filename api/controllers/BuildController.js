7b24041ef42f039d0fde6a0d3c4c1622350db64b
ndoniyor <dnimatullo@gmail.com>
2023-05-02 20:31:41 -0400

    improved controller code readability

--- api/controllers/BuildController.js
+++ api/controllers/BuildController.js
@@ -15,25 +15,25 @@ const getRandomMagicType = () => {
     return magicTypes[Math.floor(Math.random() * magicTypes.length)]
 }
 
-//issue here, when i put this all in a function, the response is weird
 const getMeleeWeapons = async (meleeType) => {
     let weaponsList = [];
-    let weapons = [];
     const type = (meleeType === "rand") ? getRandomMeleeType() : meleeType
     switch (type) {
         case "powerstance":
             console.log("ps")
-            weaponsList.push(await build.getWepsSameTyped('weapon1."type" = weapon2."type"'));
+            weaponsList = await build.getWepsSameTyped('weapon1."type" = weapon2."type"');
             break;
         case "dualwield":
             console.log("dw")
-            weaponsList.push(await build.getWepsSameTyped('weapon1."type" != weapon2."type"'));
+            weaponsList = await build.getWepsSameTyped('weapon1."type" != weapon2."type"');
             break;
         case "shield":
-            weaponsList.push(await build.getItemsDiffTable("weapons", "shields"));
-            break
+            console.log("sh");
+            weaponsList = await build.getItemsDiffTable("weapons", "shields");
+            break;
         case "singlewield":
-            weaponsList.push(await build.getItem("weapons"));
+            console.log("sw");
+            weaponsList[0] = await build.getItem("weapons");
             break;
     }
     return weaponsList;
@@ -41,6 +41,8 @@ const getMeleeWeapons = async (meleeType) => {
 
 const generateBuild = async (req, res) => {
     let completedBuild = {};
+    let ashesOfWar = [];
+    let buildType, meleeType, magicType, weapons, armor, magic;
     const params = req.body;
 
     const startingClass = await build.getItem("classes")
@@ -49,25 +51,37 @@ const generateBuild = async (req, res) => {
     const spiritAsh = (await build.getItem("spirits"))
     completedBuild["spiritAsh"] = spiritAsh;
 
-    const buildType = (params.buildType === "rand") ?
-        getRandomBuildType() : params.buildType;
+    buildType = (params.buildType === "rand") ? getRandomBuildType() : params.buildType;
+    meleeType = (params.meleeType)
+    if (buildType === "magic" && params.buildType === "rand")
+        params["magicType"] = "rand";
 
-    const weapons = (buildType === "melee") ?
-        (params.meleeType === "rand") ?
-            (await getMeleeWeapons(getRandomMeleeType())) : (await getMeleeWeapons(params.meleeType)) :
-        (await getMeleeWeapons("singlewield"))
-    completedBuild["weapons"] = weapons[0];
+    if (buildType === "melee")
+        meleeType = (params.meleeType === "rand") ? getRandomMeleeType() : params.meleeType;
+    else
+        meleeType = "singlewield";
+    weapons = await getMeleeWeapons(meleeType);
+    console.log(weapons);
+    completedBuild["weapons"] = weapons;
 
-    const armor = (params.armorSets) ?
-        (await build.getItem("armor_sets")) : (await build.getDiffArmors());
+    for (const [i, weapon] of weapons.entries()) {
+        console.log("index: ", weapon.name)
+        if (weapon.aow) {
+            console.log("looking")
+            ashesOfWar[i] = (await build.getWhere("ashes", `"type" LIKE '%${weapon.type}%'`, 1))[0]
+            delete ashesOfWar[i].type;
+        }
+    }
+    completedBuild["ashesOfWar"] = ashesOfWar;
+
+    armor = (params.armorSets) ? (await build.getItem("armor_sets")) : (await build.getDiffArmors());
     completedBuild["armor"] = armor;
 
     if (buildType === "magic") {
-        var magic = (params.magicType === "rand") ?
-            (await build.getWhere("spells", `"type"='${getRandomMagicType()}'`, "5")) : (await build.getWhere("spells", `"type"='${params.magicType}'`, "5"));
+        magicType = (params.magicType === "rand") ? getRandomMagicType() : params.magicType;
+        magic = await build.getWhere("spells", `"type"='${magicType}'`, "5");
         completedBuild["magic"] = magic;
     }
-
     res.send(completedBuild)
 }
 
