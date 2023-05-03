cf7dc6531cfa4aaae2f64c36befeae13e4d8a193
ndoniyor <dnimatullo@gmail.com>
2023-05-02 20:33:02 -0400

    added logic to dynamically resize armor container

--- client/src/components/ArmorCard.jsx
+++ client/src/components/ArmorCard.jsx
@@ -6,35 +6,37 @@ import { Container, Row, Col } from 'react-bootstrap';
 import "./styles/css/ArmorCard.css";
 
 export default function ArmorCard(props) {
+  const isTopContainerWide = props.armor.length > 1;
   const [armor, setArmor] = useState([])
 
   useEffect(() => {
-    console.log(props.armor)
     setArmor(props.armor);
   }, [props])
 
   return (
-    <Container>
-      <Row>
-        {armor.name ?
-          <Col>
-            <GridItem
-              name={armor.name}
-              image={armor.image}
-            />
-          </Col> : 
-          Object.keys(armor).length > 0 &&
-          armor.map((armorPiece) => {
-            return (
-              <Col>
-                <GridItem
-                  name={armorPiece.name}
-                  image={armorPiece.image}
-                />
-              </Col>)
-          })
-        }
-      </Row>
-    </Container>
+    <div className='container-wrapper'>
+      <Container className={`container ${isTopContainerWide ? '' : 'with-one-item'}`}>
+        <Row>
+          {armor.name ?
+            <Col>
+              <GridItem
+                name={armor.name}
+                image={armor.image}
+              />
+            </Col> :
+            Object.keys(armor).length > 0 &&
+            armor.map((armorPiece) => {
+              return (
+                <Col>
+                  <GridItem
+                    name={armorPiece.name}
+                    image={armorPiece.image}
+                  />
+                </Col>)
+            })
+          }
+        </Row>
+      </Container>
+    </div>
   )
 }
