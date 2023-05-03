035d5f31e8196ee2950b1649c2f3e8215b147c15
ndoniyor <dnimatullo@gmail.com>
2023-05-02 20:33:25 -0400

    began implementing main card for wep/class/aow

--- client/src/components/MainCard.jsx
+++ client/src/components/MainCard.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Container, Image } from 'react-bootstrap';
+import GridItem from './GridItem';
 
-export default function MainCard() {
+
+//TODO
+export default function MainCard(props) {
   return (
-    <div>MainCard</div>
+    <Container className="d-flex justify-content-center">
+      <div className="wep-container">
+        {props.weapons.map((weapon)=>{
+          return(<GridItem
+            name={weapon.name}
+            image={weapon.image}
+          />)
+        })}
+      </div>
+      <Image
+        src={props.startingClass.image}
+      />
+      <div className="wep-container">
+        {props.ashesOfWar.length>0 && props.ashesOfWar.map((ash)=>{
+          return(<GridItem
+            name={ash.name}
+            image={ash.image}
+          />)
+        })}
+      </div>
+
+    </Container>
   )
 }
