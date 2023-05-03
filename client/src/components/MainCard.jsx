bb7c4c50d43972c9584fa5146aa0e2aed5120cf2
ndoniyor <dnimatullo@gmail.com>
2023-05-03 17:24:41 -0400

    changed ordering of components for visual clarity

--- client/src/components/MainCard.jsx
+++ client/src/components/MainCard.jsx
@@ -1,31 +1,42 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { Container, Image } from 'react-bootstrap';
 import GridItem from './GridItem';
-
+import './styles/css/MainCard.css'
 
 //TODO
 export default function MainCard(props) {
+  useEffect(() => {
+    console.log("props:", props);
+  }, [])
+
   return (
-    <Container className="d-flex justify-content-center">
-      <div className="wep-container">
-        {props.weapons.map((weapon)=>{
-          return(<GridItem
+    <Container className="d-flex">
+      {/* <Container className="no-border">
+        <GridItem
+          name={props.startingClass.name}
+          image={props.startingClass.image}
+        />
+      </Container> */}
+      <Container className="wep-container">
+        {props.weapons.map((weapon) => {
+          return (<GridItem
             name={weapon.name}
             image={weapon.image}
           />)
         })}
-      </div>
-      <Image
-        src={props.startingClass.image}
-      />
-      <div className="wep-container">
-        {props.ashesOfWar.length>0 && props.ashesOfWar.map((ash)=>{
-          return(<GridItem
-            name={ash.name}
-            image={ash.image}
-          />)
-        })}
-      </div>
+      </Container>
+      {props.ashesOfWar.length > 0 &&
+        <Container className="wep-container">
+          {props.ashesOfWar.map((ash) => {
+            if (ash)
+              return (<GridItem
+                name={ash.name}
+                image={ash.image}
+              />)
+          })}
+        </Container>
+      }
 
     </Container>
   )
